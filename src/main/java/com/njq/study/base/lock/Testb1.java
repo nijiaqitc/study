f3fbe2579359b1de6f578a161d4966690847fa0f
njq <A23456>
2020-06-18 00:14:56 +0800

    wewe11

--- src/main/java/com/njq/study/base/lock/Testb1.java
+++ src/main/java/com/njq/study/base/lock/Testb1.java
@@ -1,4 +1,4 @@
-package com.njq.study;
+package com.njq.study.base.lock;
 
 import java.util.concurrent.TimeUnit;
 
@@ -19,16 +19,32 @@ public class Testb1 {
     }
 
 
-    public synchronized void getvalue(String a){
+    public synchronized void getvalue(String a) throws  Exception{
         try {
+            System.out.println("valuesleep0");
             Thread.sleep(1000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+        throw new Exception("11");
+//        System.out.println("value"+a);
+    }
+
+    public synchronized void getvalu1(String a){
         System.out.println("value"+a);
     }
 
     public void getv1(){
         System.out.println("v1");
+    }
+
+    public void getv2(String a){
+        try {
+            System.out.println("valuesleep");
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("value"+a);
     }
 }
